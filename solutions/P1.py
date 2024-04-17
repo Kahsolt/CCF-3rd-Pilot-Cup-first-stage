a3557f13e12105e47a9b4e7f28a218bf5dcbbb37
Kahsolt <kahsolt@qq.com>
2024-04-17 13:23:36 +0800

    add WState_qiskit_VQC

--- solutions/P1.py
+++ solutions/P1.py
@@ -92,6 +92,9 @@ if 'vqi':
     U1 as U1_original,
     U2 as U2_original,
     U3 as U3_original,
+    CRX as CRX_original,
+    CRY as CRY_original,
+    CRZ as CRZ_original,
     CR as CR_original,
     # trainable variational circuit modules
     #   .forward(q_machine:QMachine)
@@ -113,6 +116,9 @@ if 'vqi':
     U1  = lambda wires, **kwargs: U1_original (has_params=True, trainable=True, wires=wires, **kwargs)
     U2  = lambda wires, **kwargs: U2_original (has_params=True, trainable=True, wires=wires, **kwargs)
     U3  = lambda wires, **kwargs: U3_original (has_params=True, trainable=True, wires=wires, **kwargs)
+    CRX = lambda wires, **kwargs: CRX_original(has_params=True, trainable=True, wires=wires, **kwargs)
+    CRY = lambda wires, **kwargs: CRY_original(has_params=True, trainable=True, wires=wires, **kwargs)
+    CRZ = lambda wires, **kwargs: CRZ_original(has_params=True, trainable=True, wires=wires, **kwargs)
     CR  = lambda wires, **kwargs: CR_original (has_params=True, trainable=True, wires=wires, **kwargs)   # 受控相位旋转
 
     class CU3(Module):    # 受控任意角度旋转
@@ -120,7 +126,7 @@ if 'vqi':
         super().__init__()
         assert len(wires) == 2 and all([isinstance(wire, int) for wire in wires])
         self.wires = wires
-        self.params = Parameter(shape=[3], initializer=zeros, dtype=kcomplex64)
+        self.params = Parameter(shape=[3], initializer=zeros, dtype=kfloat32)
       def forward(self, vqm:QMachine):
         crx(vqm, wires=self.wires, params=self.params[0:1])
         cry(vqm, wires=self.wires, params=self.params[1:2])
@@ -159,7 +165,7 @@ class Model(Module):
     ckpt_dict: StateDict = load_parameters(fp)
     state_dict = self.state_dict()
     if 'check keys':
-      missing_keys = state_dict.keys() - ckpt_dict.keys() - {'vqm.states', 'vqm.state'}
+      missing_keys = state_dict.keys() - ckpt_dict.keys()
       if DEBUG and missing_keys: print('>> missing_keys:', missing_keys)
       redundant_keys = ckpt_dict.keys() - state_dict.keys()
       if DEBUG and redundant_keys: print('>> redundant_keys:', redundant_keys)
@@ -167,9 +173,7 @@ class Model(Module):
     self.load_state_dict(state_dict)
 
   def save_ckpt(self, fp:Path):
-    state_dict: StateDict = self.state_dict()
-    param_dict = {k: v for k, v in state_dict.items() if k.endswith('.params')}
-    save_parameters(param_dict, fp)
+    save_parameters(self.state_dict(), fp)
 
   def __str__(self) -> str:
     return self.__class__.__name__
@@ -348,9 +352,9 @@ class CCQC(Model):
   def __init__(self, n_qubits:int):
     super().__init__(n_qubits)
 
-    self.rot1   = ModuleList([U3(wires=i) for i in range(n_qubits)])
-    self.entgl1 = ModuleList([CU3(wires=[(i+1)%n_qubits, i]) for i in reversed(range(n_qubits))])
-    self.rot2   = ModuleList([U3(wires=i) for i in range(n_qubits)])
+    self.rot1   = ModuleList(U3(wires=i) for i in range(n_qubits))
+    self.entgl1 = ModuleList(CU3(wires=[(i+1)%n_qubits, i]) for i in reversed(range(n_qubits)))
+    self.rot2   = ModuleList(U3(wires=i) for i in range(n_qubits))
     entgl2, ctrl_bit = [], 0
     offset = 3 if n_qubits >= 4 else 1
     for _ in range(n_qubits):
@@ -370,6 +374,112 @@ class CCQC(Model):
     for entgl in self.entgl2: entgl.forward(vqm)
 
 
+class WState_VQC(Model):
+
+  ''' parametrized WState_* method '''
+
+  def __str__(self) -> str:
+    return super().__str__() + f'_Q={self.n_qubits}'
+
+class WState_qiskit_VQC0(WState_VQC):
+
+  ''' parametrizing per-theta in WState_qiskit '''
+
+  def __init__(self, n_qubits:int):
+    super().__init__(n_qubits)
+
+    self.thetas = Parameter(shape=[n_qubits-1], initializer=quantum_uniform, dtype=kfloat32)
+
+  def init_state(self):
+    vqm = self.vqm
+    nq = self.n_qubits
+    paulix(vqm, wires=nq-1)
+
+  def F_gate(self, i:int, j:int):
+    vqm = self.vqm
+    ry(vqm, wires=j, params=-self.thetas[j:j+1])
+    cz(vqm, wires=[j, i])
+    ry(vqm, wires=j, params=self.thetas[j:j+1])
+
+  def forward(self):
+    vqm = self.vqm
+    nq = self.n_qubits
+    self.init_state()
+    for i in range(nq - 1, 0, -1):    # F(n-1, n)
+      self.F_gate(i, i - 1)
+    for i in range(nq - 1, 0, -1):    # reversed CNOT(n, n-1)
+      cnot(vqm, wires=[i - 1, i])
+
+class WState_qiskit_VQC1(WState_qiskit_VQC0):
+
+  ''' parametrizing per-theta + init_state in WState_qiskit '''
+
+  def __init__(self, n_qubits:int):
+    super().__init__(n_qubits)
+
+    self.theta0 = Parameter(shape=[1], initializer=quantum_uniform, dtype=kfloat32)
+
+  def init_state(self):
+    vqm = self.vqm
+    nq = self.n_qubits
+    ry(vqm, wires=nq-1, params=self.theta0)    # X -> RY
+
+class WState_qiskit_VQC2(WState_VQC):
+
+  ''' parametrizing per-rot-gate in WState_qiskit '''
+
+  def __init__(self, n_qubits:int):
+    super().__init__(n_qubits)
+
+    self.ry_0 = RY(wires=n_qubits-1)
+    self.ry_n = ModuleList(RY(wires=i) for i in range(n_qubits - 1))
+    self.ry_p = ModuleList(RY(wires=i) for i in range(n_qubits - 1))
+
+  def F_gate(self, i:int, j:int):
+    vqm = self.vqm
+    self.ry_n[j](q_machine=vqm)
+    cz(vqm, wires=[j, i])
+    self.ry_p[j](q_machine=vqm)
+
+  def forward(self):
+    vqm = self.vqm
+    nq = self.n_qubits
+    self.ry_0(q_machine=vqm)          # X -> RY
+    for i in range(nq - 1, 0, -1):    # F(n-1, n)
+      self.F_gate(i, i - 1)
+    for i in range(nq - 1, 0, -1):    # reversed CNOT(n, n-1)
+      cnot(vqm, wires=[i - 1, i])
+
+class WState_qiskit_VQC3(WState_qiskit_VQC2):
+
+  ''' parametrizing per-gate in WState_qiskit '''
+
+  def __init__(self, n_qubits:int):
+    super().__init__(n_qubits)
+
+    self.crz = ModuleList(CRZ(wires=[i, i+1]) for i in range(n_qubits - 1))
+    self.crx = ModuleList(CRX(wires=[i, i+1]) for i in range(n_qubits - 1))
+
+  def F_gate(self, i:int, j:int):
+    vqm = self.vqm
+    self.ry_n[j](q_machine=vqm)
+    self.crz [j](q_machine=vqm)
+    self.ry_p[j](q_machine=vqm)
+
+  def forward(self):
+    vqm = self.vqm
+    nq = self.n_qubits
+    self.ry_0(q_machine=vqm)          # X -> RY
+    for i in range(nq - 1, 0, -1):    # F(n-1, n)
+      self.F_gate(i, i - 1)
+    for i in range(nq - 1, 0, -1):    # reversed CNOT(n, n-1)
+      self.crx[i - 1](q_machine=vqm)
+
+
+''' Trainer '''
+
+MODELS = {k: v for k, v in globals().items() if type(v) == type(Model) and issubclass(v, Model) and v not in [Model, AnsatzModel, WState_VQC]}
+
 def get_truth(n_qubits:int) -> QTensor:
   pdist = tensor.zeros([2**n_qubits], dtype=kfloat32)
   for i in range(n_qubits):
@@ -379,14 +489,13 @@ def get_truth(n_qubits:int) -> QTensor:
 def get_model(
     method:str, n_qubits:int, depth:int=2,
     n_iter:int=10000, loss:str='l2', optim:str='SGD', lr:float=0.2, sgd_momentum:float=0.8, sgd_nesterov:bool=False,
-    seed:int=114514,
+    seed:int=114514, train_if_no_exist:bool=True,
   ) -> Model:
 
   ''' env '''
   set_random_seed(seed)
 
   ''' model '''
-  MODELS = {k: v for k, v in globals().items() if type(v) == type(Model) and issubclass(v, Model) and v not in [Model, AnsatzModel]}
   model_cls = MODELS[method]
   env = locals()    # avoid closure :(
   kwargs = {k: env[k] for k in signature(model_cls).parameters.keys()}
@@ -394,7 +503,7 @@ def get_model(
   model.eval()
   param_cnt = sum([p.numel() for p in model.parameters() if p.requires_grad])
   if DEBUG:
-    print(f'>> [{method}] n_qubits: {model.n_qubits}, n_params: {param_cnt}')
+    print(f'>> [{method}] depth: {depth}, n_qubits: {model.n_qubits}, n_params: {param_cnt}')
     assert param_cnt == sum([p.numel() for p in model.parameters()])
   if param_cnt == 0: return model
 
@@ -403,6 +512,7 @@ def get_model(
   if ckpt_fp.exists():
     model.load_ckpt(ckpt_fp)
     return model
+  elif not train_if_no_exist: return
 
   ''' data '''
   pdist = get_truth(n_qubits)
@@ -429,9 +539,11 @@ def get_model(
     loss.backward()
     optim._step()
 
+    kl = kl_div(pdist.numpy(), qdist.numpy()).sum()
     if DEBUG and steps % 200 == 0:
       ema_loss = (0.4 * ema_loss + 0.6 * loss.item()) if ema_loss is not None else loss.item()
-      print(f'[Epoch {steps}/{n_iter}] kl_div: {kl_div(pdist.numpy(), qdist.numpy()).sum():.5f}, l1_loss: {MAE(pdist, qdist):.5g}, ema(loss): {ema_loss:.5g}')
+      print(f'[Epoch {steps}/{n_iter}] kl_div: {kl:.5f}, l1_loss: {MAE(pdist, qdist):.5g}, ema(loss): {ema_loss:.5g}')
+    if kl < 1e-8: break   # converge early
   model.eval()
 
   ''' save '''
@@ -446,30 +558,20 @@ def question1(n_qubits:int) -> ProbDict:
   ''' Special case for n_qubits = 4 '''
   assert n_qubits == 4
 
-  method = 'WState_qiskit'
-  depth = 2
-  n_iter = 10000
-  loss = 'l2'
-  optim = 'SGD'
-  lr = 0.2
-  seed = 114514
+  method = 'WState_qiskit_VQC0'
+  depth = -1
 
-  model = get_model(method, n_qubits, depth, n_iter, loss, optim, lr, seed=seed)
+  model = get_model(method, n_qubits, depth)
   result = model.get_prob_wstate()
   return result
 
 def question2(n_qubits:int) -> ProbDict:
   ''' Arbitary case for n_qubits '''
 
-  method = 'WState_qiskit'
-  depth = 2
-  n_iter = 10000
-  loss = 'l2'
-  optim = 'SGD'
-  lr = 0.2
-  seed = 114514
+  method = 'WState_qiskit_VQC0'
+  depth = -1
 
-  model = get_model(method, n_qubits, depth, n_iter, loss, optim, lr, seed=seed)
+  model = get_model(method, n_qubits, depth)
   result = model.get_prob_wstate()
   return result
 
@@ -488,9 +590,12 @@ def timer(fn):
 
 @timer
 def pretrain(n_qubits:int):
+  global DEBUG
+  DEBUG = True
+
   # TODO: tune these to find the optimal setting
-  method = 'CCQC'
-  depth = 2
+  method = 'WState_qiskit_VQC0'
+  depth = 1
   n_iter = 10000
   loss = 'l2'
   optim = 'SGD'
@@ -501,39 +606,53 @@ def pretrain(n_qubits:int):
   result = model.get_prob_wstate()
   print(result)
 
-
 @timer
-def benchmark(n_qubits_cases:List[int]):
-  METHODS = [k for k, v in globals().items() if type(v) == type(Model) and issubclass(v, Model) and v not in [Model, AnsatzModel]]
-  METHODS = [
-    #'WState3_wikipedia', 
-    #'WState4_toffoli', 
-    'WState_qiskit', 
-    'WState_diogo', 
-    'BET', 
-    'SET', 
-    'HEA', 
-    'EEA', 
-    'CCQC',
-  ]
+def benchmark(n_qubits_cases:List[int], train_if_no_exist:bool=False):
+  global DEBUG
+  DEBUG = False
+
   TRUTHS = { n_qubits: get_truth(n_qubits) for n_qubits in n_qubits_cases }
   mean = lambda x: sum(x) / len(x) if len(x) else 0.0
-  for depth in [2, 1, 3]:
-    for method in METHODS:
+
+  def run_depth_group(depth:int, methods:List[str]):
+    print('=' * 32 + f' [D={depth}] ' + '=' * 32)
+    for method in methods:
       error_list = []
       for n_qubits in n_qubits_cases:
-        model = get_model(method, n_qubits, depth)
-        qdist = model.get_prob()
-        if qdist.shape[0] != 2**n_qubits: continue    # compat for fixed n_qubits methods
-        pdist = TRUTHS[n_qubits]
-        err = MAE(pdist, qdist).item()
+        model = get_model(method, n_qubits, depth, train_if_no_exist=train_if_no_exist)
+        if model is not None:
+          qdist = model.get_prob()
+          pdist = TRUTHS[n_qubits]
+          err = MAE(pdist, qdist).item()
+        else:
+          err = float('inf')
         error_list.append(err)
       print(f'[{method}] {mean(error_list):.5f} | {[round(e, 4) for e in error_list]}')
+    print()
+
+  FIX_DEPTH_METHODS = [
+    'WState_diogo',
+    'WState_qiskit',
+    'WState_qiskit_VQC0',
+    'WState_qiskit_VQC1',
+    'WState_qiskit_VQC2',
+    'WState_qiskit_VQC3',
+    'CCQC',
+  ]
+  run_depth_group(-1, FIX_DEPTH_METHODS)
+
+  VAR_DEPTH_METHODS = [
+    'BET',
+    'SET',
+    'HEA',
+    'EEA',
+  ]
+  for depth in [1, 2, 3]:
+    run_depth_group(depth, VAR_DEPTH_METHODS)
 
 
 if __name__ == '__main__':
-  DEBUG = True
   n_qubits_cases = [3, 4, 5, 6, 8, 10]
   for n_qubits in n_qubits_cases:
     pretrain(n_qubits)
-  benchmark(n_qubits_cases)
+  benchmark(n_qubits_cases, train_if_no_exist=False)
